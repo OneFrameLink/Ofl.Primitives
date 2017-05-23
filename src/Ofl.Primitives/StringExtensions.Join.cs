b8d737b6df5a29774562f0c4595841611d4f858d
casperOne <casperOne@caspershouse.com>
2017-05-23 14:41:42 -0400

    Added Join to StringExtensions.

---
+++ src/Ofl.Primitives/StringExtensions.Join.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ofl
+{
+    public static partial class StringExtensions
+    {
+        public static string Join(this IEnumerable<string> values, string separator)
+        {
+            // Validate parameters.
+            if (values == null) throw new ArgumentNullException(nameof(values));
+
+            // NOTE: Don't check separator, as Join has specific rules around that.
+
+            // Join.
+            return string.Join(separator, values);
+        }
+    }
+}
