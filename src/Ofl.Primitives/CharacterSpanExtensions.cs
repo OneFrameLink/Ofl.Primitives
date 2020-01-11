2704ed5530d9a4b86bb0a2c43dadca3efebf1a09
casperOne <casperOne@caspershouse.com>
2020-01-11 14:41:44 -0500

    Added extensions for character spans.

---
+++ src/Ofl.Primitives/CharacterSpanExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ofl
+{
+    public static partial class CharacterSpanExtensions
+    {
+        public static Span<char> ToCamelCaseInvariant(this Span<char> value) => value.ToCamelCase(CultureInfo.InvariantCulture);
+
+        public static Span<char> ToCamelCase(this Span<char> value) => value.ToCamelCase(CultureInfo.CurrentCulture);
+
+        public static Span<char> ToCamelCase(this Span<char> value, CultureInfo cultureInfo)
+        {
+            // If the length is 0, return.
+            if (value.Length == 0) return value;
+
+            // Get the character.
+            char firstCharacter = value[0];
+
+            // If lower case already, then get out.
+            if (char.IsLower(firstCharacter)) return value;
+
+            // Update.
+            value[0] = cultureInfo.TextInfo.ToLower(firstCharacter);
+
+            // Return the span.
+            return value;
+        }
+    }
+}
---
+++ test/Ofl.Primitives.Tests/CharacterSpanExtensionsTests_ToCamelCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace Ofl.Primitives.Tests
+{
+    public class CharacterSpanExtensionsTests_ToCamelCase
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("hello", "hello")]
+        [InlineData("Hello", "hello")]
+        [InlineData("helloThere", "helloThere")]
+        [InlineData("HelloThere", "helloThere")]
+        public void ToCamelCase(string test, string expected)
+        {
+            // Create a span.
+            ReadOnlySpan<char> original = test;
+
+            // Allocate a new span.
+            Span<char> actual = stackalloc char[original.Length];
+
+            // Copy to.
+            original.CopyTo(actual);
+
+            // Update.
+            actual = actual.ToCamelCase();
+
+            // Compare actual to expected.
+            Assert.True(actual.SequenceEqual(expected));
+        }
+    }
+}
